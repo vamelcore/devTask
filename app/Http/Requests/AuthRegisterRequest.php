4871018803834affcc78422d8dde246a2a7d5793
VITALIY <vamelcore@gmail.com>
2022-06-06 17:40:25 +0300

    Feat: added authorization form request classes and Validation rule FullName

---
+++ app/Http/Requests/AuthRegisterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\FullName;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', new FullName(2)],
+            'email' => ['required','email:rfc,dns','unique:users'],
+            'password' => ['required','confirmed','min:8'],
+        ];
+    }
+}
