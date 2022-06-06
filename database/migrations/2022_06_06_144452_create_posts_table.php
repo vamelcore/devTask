2e582bd6260b704db3feea87f4aa4b310997462e
VITALIY <vamelcore@gmail.com>
2022-06-06 18:00:38 +0300

    Migration: posts

---
+++ database/migrations/2022_06_06_144452_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('text');
+            $table->string('image');
+            $table->string('url');
+            $table->boolean('active')->default(0);
+            $table->integer('sort_order');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
