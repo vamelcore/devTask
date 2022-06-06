e39d9953501466fe6c9a29a9f81ed8616f950259
VITALIY <vamelcore@gmail.com>
2022-06-06 17:41:48 +0300

    Feat: added authorization routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::middleware("guest:web")->group(function () {
+    Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+    Route::post('/login_process', [\App\Http\Controllers\AuthController::class, 'loginProcess'])->name('login_process');
+
+    Route::get('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
+    Route::post('/register_process', [\App\Http\Controllers\AuthController::class, 'registerProcess'])->name('register_process');
+});
+
+Route::middleware("auth:web")->group(function () {
+    Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
 });
