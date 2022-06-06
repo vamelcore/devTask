879a269bd729d01bdaa6b463b6a2ba7c089ea347
VITALIY <vamelcore@gmail.com>
2022-06-06 21:18:43 +0300

    Feat: added Post views

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layout.app')
+
+@section('title', 'Posts List')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Posts List</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('posts.create') }}"> Create New Post</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>Title</th>
+            <th>Url</th>
+            <th>Text</th>
+        </tr>
+        @foreach ($posts as $post)
+            <tr>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->url }}</td>
+                <td>{{ $post->text }}</td>
+                <td>
+                    <a class="btn btn-info" href="{{ route('posts.show', $post->id) }}">Show</a>
+                    <a class="btn btn-primary" href="{{ route('posts.edit', $post->id) }}">Edit</a>
+                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.app')
+
+@section('title', 'Show Post')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Show Post</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('posts.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $post->title }}
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Url:</strong>
+                {{ $post->url }}
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Text:</strong>
+                {{ $post->text }}
+            </div>
+        </div>
+
+    </div>
+@endsection
